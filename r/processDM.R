54222c2179e479a289ba699b650ba5eb5d4eb339
NovasTaylor <NovasTaylor@gmail.com>
2017-01-06 09:36:41 -0500

    dataCreate.R code moved to processDM.R

--- r/processDM.R
+++ r/processDM.R
@@ -12,6 +12,33 @@
 # TODO : 
 ###############################################################################
 
+#-- Data Creation for testing purposes. --------------------------------------- 
+# Birthdate 
+# NOTE: Date calculations based on SECONDS so you must convert the age in Years to seconds
+dm$brthdate <- strptime(strptime(dm$rfstdtc, "%Y-%m-%d") - (strtoi(dm$age) * 365.25 * 24 * 60 * 60), "%Y-%m-%d")
+# AO's formula: BRTHDTC = RFSTDTC - AGE x 365.25   
+
+
+# Informed Consent  (column present with missing values in DM source).
+dm$rficdtc <- dm$dmdtc
+
+#-- Create Data required for prototyping
+# Investigator name and ID not present in source data
+dm$invnam <- 'Jones'
+dm$invid  <- '123'
+
+
+# Set Death values for Person_1
+dm$dthfl[dm$personNum == 1 ] <- "Y"
+
+# unfactorize the dthdtc column to allow entry of a bogus date
+dm$dthdtc <- as.character(dm$dthdtc)
+
+temp <-as.Date("2013-12-26", "%Y-%m-%d")
+dm$dthdtc[dm$personNum == 1 ] <- "2013-12-26"
+
+#-- End Data Creation ---------------------------------------------------------
+
 processDM<-function()
 {    
     # Loop through the dm dataframe and create the triples for each 
