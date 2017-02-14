f633c87c524944506a496395001ecababecadd47
NovasTaylor <NovasTaylor@gmail.com>
2017-02-14 11:04:44 -0500

    combining processing of Date and Age fields into single script.

--- r/createFrag.R
+++ r/createFrag.R
@@ -1,19 +1,29 @@
 ###############################################################################
-# FILE: dateDict.R
-# DESC: Create a dicitionary table of dates and their fragments that will 
-#         become part of date URI's in the TTL.
+# FILE: createFrag.R
+# DESC: Create URI fragments for Dates, Ages and other fields that are shared 
+#         in common between various resources. Eg: A date (DATE_1) may be both
+#         a study start date and a product administrationg date. 
 #       dateKey - used to merge back into a column
 #       dateFrag - the fragment that will become part of a URI for a date col
+#
+#       ageKey - used to merge back into a column
+#       ageFrag - the fragment that will become part of a URI for a age col
+#
 # REQ : 
 #       
 # SRC : 
 # IN  : 
 # OUT : 
 # NOTE: 
-#        
-# TODO: Extend to include dates in VS
+#       Ages are all assumed to be in YEARS for this dataset. 
+# TODO: Combine the common elements into function(s) where possible
 #
 ###############################################################################
+#------------------------------------------------------------------------------
+#  Date Fragments
+#  All dates from across both DM and VS domains. 
+#  TODO: Add additional domains as project scope expands.
+#------------------------------------------------------------------------------
 # dm dates
 dmDates <- dm[,c("rfstdtc", "rfendtc", "rfxstdtc","rfxendtc", "rficdtc", "rfpendtc", "dthdtc", "dmdtc", "brthdate")]
 
@@ -43,7 +53,6 @@ dateList$dateFrag <- paste0("Date_", 1:nrow(dateList))   # Generate a list of ID
 
 dateDict <- dateList[,c("dateKey", "dateFrag")]
 
-
 # Merge in the dateKey value to created a coded version of the date field, naming
 #    the column with a _Frag suffix.
 addDateFrag<-function(domainName, colName)
@@ -71,4 +80,39 @@ dm <- addDateFrag(dm, "brthdate")
 
 dmDatesList <- c("rfstdtc", "rfendtc", "rfxstdtc","rfxendtc", "rficdtc", "rfpendtc", "dthdtc", "dmdtc", "brthdate")
 
+#------------------------------------------------------------------------------
+#  Age Fragments
+#------------------------------------------------------------------------------
+# There is only 1 column of ages in the study
+ageKey <- dm$age
 
+ageList <-as.data.frame(ageKey)
+# Remove duplicates  ERROR: Changes from dataframe here!!! 
+ageList <- unique(ageList
+                  )
+# Remove missing(blank) ages by coding to NA and them omitting
+ageList[ageList==""] <- NA
+ageList <- na.omit(ageList)
+
+# Sort by age. Use dplyr arrange instead of order to avoid loss of df type
+ageList <- arrange(ageList, ageKey)
+
+# Create the coded value for each age as Age_n 
+ageList$ageFrag <- paste0("AgeMeasurement_", 1:nrow(ageList))   # Generate a list of ID numbers
+
+ageDict <- ageList[,c("ageKey", "ageFrag")]
+
+# Merge in the ageKey value to created a coded version of the age field, naming
+#    the column with a _Frag suffix.
+addAgeFrag<-function(domainName, colName)
+{
+    withFrag <- merge(x = ageDict, y = domainName, by.x="ageKey", by.y=colName, all.y = TRUE)
+    # Rename the merged-in key value to the original column name to preserve original data
+    names(withFrag)[names(withFrag)=="ageKey"] <-  colName
+    # Rename ageFrag value to coded value using colname +  _Frag suffix
+    names(withFrag)[names(withFrag)=="ageFrag"] <- paste0(colName, "_Frag")
+    return(withFrag)
+}
+
+# Add the age Fragment back into the DM dataframe.
+dm <- addAgeFrag(dm, "age")  
