f5cd1c3af4bff30f287513f6ac619f510b903302
NovasTaylor <NovasTaylor@gmail.com>
2017-04-18 10:04:15 -0400

    code tidy

--- r/graphMeta.R
+++ r/graphMeta.R
@@ -1,13 +1,13 @@
 #########################################################################
 # NAME  : graphMeta.R 
 # AUTH  : Tim W.
-# DESCR : Create the RDF Triples for metadata 
+# DESCR : Create the RDF Triples for graph metadata 
 #             
 # NOTES : 
 # IN    : Input of raw data with minor massaging occurs in buildRDF-Driver.R
 # OUT   : 
 # REQ   : 
-# TODO  : CLean up the code so dates are only written to the file when the value
+# TODO  : Clean up the code so dates are only written to the file when the value
 #            is non-missing. Now writes N/A, which is good for debugging but 
 #            little else.
 ###############################################################################
@@ -19,57 +19,56 @@
 
 #-- Part 1:  Graph Metadata
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.RDFS, "comment"),
-                paste0("Example SDTM data converted from CDISC SDTM to RDF Graph as part of the SDTM Data to RDF project."), type="string"
-)
-
-add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.RDFS, "label"),
-                paste0("SDTM data as a graph."), type="string"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.RDFS, "comment"),
+    paste0("Example SDTM data converted from CDISC SDTM to RDF Graph as part of the SDTM Data to RDF project."), type="string"
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.DCTERMS, "description"),
-                paste0("Data converted from the CDISCPILOT01 SDTM DM domain to RDF."), type="string"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.RDFS, "label"),
+    paste0("SDTM data as a graph."), type="string"
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.DCTERMS, "title"),
-                paste0("SDTM data as RDF."), type="string"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.DCTERMS, "description"),
+    paste0("Data converted from the CDISCPILOT01 SDTM DM domain to RDF."), type="string"
+)
+add.data.triple(store,
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.DCTERMS, "title"),
+    paste0("SDTM data as RDF."), type="string"
 )
 # Later change these to link to FOAF description of the people as separate resources
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.DCTERMS, "contributor"),
-                paste0("Tim Williams"), type="string"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.DCTERMS, "contributor"),
+    paste0("Tim Williams"), type="string"
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.DCTERMS, "contributor"),
-                paste0("Armando Oliva"), type="string"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.DCTERMS, "contributor"),
+    paste0("Armando Oliva"), type="string"
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.PAV, "createdOn"),
-                paste0(gsub("(\\d\\d)$", ":\\1",strftime(Sys.time(),"%Y-%m-%dT%H:%M:%S%z"))), type="dateTime"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.PAV, "createdOn"),
+    paste0(gsub("(\\d\\d)$", ":\\1",strftime(Sys.time(),"%Y-%m-%dT%H:%M:%S%z"))), type="dateTime"
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.PAV, "createdWith"),
-                paste0("R Version ", R.version$major, ".", R.version$minor,
-                       " Platform:", R.version$platform, " swith scripts from SDTM Data to RDF Working group"), type="string"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.PAV, "createdWith"),
+    paste0("R Version ", R.version$major, ".", R.version$minor,
+        " Platform:", R.version$platform, " swith scripts from SDTM Data to RDF Working group"), type="string"
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.PAV, "version"),
-                paste0(version), type="string"
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.PAV, "version"),
+    paste0(version), type="string"
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "sdtm-graph"),
-                paste0(prefix.DCAT, "distribution"),
-                paste0(outFilename)
+    paste0(prefix.CDISCPILOT01, "sdtm-graph"),
+    paste0(prefix.DCAT, "distribution"),
+    paste0(outFilename)
 )
 #TODO:  Reinstate, listing source folder of files instead of indivdual files?
 #add.data.triple(store,
@@ -77,21 +76,22 @@ add.data.triple(store,
 #    paste0(prefix.PROV, "wasDerivedFrom"),
 #    paste0(inFilename)
 #)
+
 #-- Part 2: For values created only once:
 #TODO: MOVE THIS TO THE CODELIST R SCRIPT.
 #  Value is hard coded here. Make it data driven.
 add.triple(store,
-           paste0(prefix.CDISCPILOT01, "study-CDISCPILOT01"),
-           paste0(prefix.RDF,"type" ),
-           paste0(prefix.STUDY,"Study")
+    paste0(prefix.CDISCPILOT01, "study-CDISCPILOT01"),
+    paste0(prefix.RDF,"type" ),
+    paste0(prefix.STUDY,"Study")
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "study-CDISCPILOT01"),
-                paste0(prefix.STUDY,"hasStudyID" ),
-                paste0("CDISCPILOT01"), type="string" 
+    paste0(prefix.CDISCPILOT01, "study-CDISCPILOT01"),
+    paste0(prefix.STUDY,"hasStudyID" ),
+    paste0("CDISCPILOT01"), type="string" 
 )
 add.data.triple(store,
-                paste0(prefix.CDISCPILOT01, "study-CDISCPILOT01"),
-                paste0(prefix.RDFS,"label" ),
-                paste0("study-CDISCPILOT01"), type="string" 
+    paste0(prefix.CDISCPILOT01, "study-CDISCPILOT01"),
+    paste0(prefix.RDFS,"label" ),
+    paste0("study-CDISCPILOT01"), type="string" 
 )
