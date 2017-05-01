859014c204ff427b92e4ad1cd29bef2748354e8d
NovasTaylor <NovasTaylor@gmail.com>
2017-05-01 10:05:39 -0400

    reverted changes. next: adjust layout. Triple selection is hard coded

--- r/validation/CompTriples-Shiny.R
+++ r/validation/CompTriples-Shiny.R
@@ -19,79 +19,59 @@
 # 
 ###############################################################################
 library(plyr)    #  rename
+library(dplyr)   # anti_join. MUst load dplyr AFTER plyr!!
 library(reshape) #  melt
 library(rrdf)
 library(shiny)
 
-
-# Load both source files 
-#    inFile1 <<- input$file1
-#    inFile2 <<- input$file2
-    #TODO Confirm these two steps
-#    file.rename(inFile1$datapath,
-#               paste(inFile1$datapath, ".ttl", sep=""))
-#    file.rename(inFile2$datapath,
-#               paste(inFile2$datapath, ".ttl", sep=""))
-#   sourceA = load.rdf(paste(inFile1$datapath,".ttl",sep=""), format="N3")
-#   sourceB = load.rdf(paste(inFile2$datapath,".ttl",sep=""), format="N3")
-
-
-
 server <- function(input, output) {
- 
-    query <- reactive({
+    output$contents <- renderTable({ 
+    inFileR <<- input$fileR
+    inFileOnt <<- input$fileOnt
+    # Do not do anything until both FileR and FileOnt have been specified.
+    if(is.null(inFileR) | is.null(inFileOnt) )
+      return(NULL)
 
-
-        qPt1 <- 'PREFIX cdiscpilot01: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/cdiscpilot01#>
+    #TODO Confirm these two steps
+    file.rename(inFileR$datapath,
+               paste(inFileR$datapath, ".ttl", sep=""))
+    file.rename(inFileOnt$datapath,
+               paste(inFileOnt$datapath, ".ttl", sep=""))
+    
+   query = 'PREFIX cdiscpilot01: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/cdiscpilot01#>
 PREFIX custom: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/custom#>
 PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX stuy:  <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/study#>
-SELECT ?s ?p ?o '
-   
-    qPt2 <- paste0("WHERE { ", input$startTriple, " ?p ?o ." ,
-       "BIND('", input$startTriple, "' as ?s  }")  
+PREFIX study:  <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/study#>
 
-    query <- paste0(qPt1, qPt2)   
-    })
-    
+SELECT ?s ?p ?o
+WHERE { cdiscpilot01:Person_1 ?p ?o . 
+  BIND("cdiscpilot01:Person_1" as ?s)
+}'
+   sourceR = load.rdf(paste(inFileR$datapath,".ttl",sep=""), format="N3")
    # Global assign for trouble shooting
-   # triplesTW <<- as.data.frame(sparql.rdf(sourceA, query))
+   triplesR <<- as.data.frame(sparql.rdf(sourceR, query))
    
-#   triplesAO <<- as.data.frame(sparql.rdf(sourceB, query))
+   sourceOnt = load.rdf(paste(inFileOnt$datapath,".ttl",sep=""), format="N3")
+   triplesOnt <<- as.data.frame(sparql.rdf(sourceOnt, query))
 
-#   inANotB<<-anti_join(triplesTW, triplesAO)
-#   inBNotA<<-anti_join(triplesAO, triplesTW)
+   inRNotOnt<<-anti_join(triplesR, triplesOnt)
+   inBNotR<<-anti_join(triplesOnt, triplesR)
    
-   # output$inANotB <- renderTable({inANotB})
-#   inANotB
+   # output$inRNotOnt <- renderTable({inRNotOnt})
+   inRNotOnt
    # TO ADD
-   # output$inBNotA <- renderTable({inBNotA})
-#    })
-# query <-input$startTriple
-# output$query <- renderText(paste0(input$startTriple))    
-
-    # Note use of query(), not query.
-    output$query <- renderText(query())    
+   # output$inOntNotR<- renderTable({inOntNotA})
+    })
 }
 
-#------------------------------------------------------------------------------
-# UI 
-#------------------------------------------------------------------------------
 ui <- fluidPage(
-  titlePanel("SPARQL query"),
-  fluidRow(
-      textInput("startTriple", label = h4("Parent Triple"), value="cdiscpilot01:Person_1")
-  ),
-  fluidRow(
-      h3(textOutput('query', container = span))    
-  )
-  
-  # textOutput('query'),
-  # tableOutput('contents')
+  fileInput('fileR', 'TTL from R'),
+  fileInput('fileOnt', 'TTL from Ont'),
+  # visNetworkOutput("network",height = "500px"),
+  # textOutput('text1'),
+  tableOutput('contents')
 )
-
-
 shinyApp(ui = ui, server = server)
 
