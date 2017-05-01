09053e57778154bd6c1966c086f666d038e36767
NovasTaylor <NovasTaylor@gmail.com>
2017-05-01 09:11:20 -0400

    query substitution formed. Query validity not checked.

--- r/validation/CompTriples-Shiny.R
+++ r/validation/CompTriples-Shiny.R
@@ -23,54 +23,75 @@ library(reshape) #  melt
 library(rrdf)
 library(shiny)
 
-server <- function(input, output) {
-    output$contents <- renderTable({ 
-    inFile1 <<- input$file1
-    inFile2 <<- input$file2
-    # Do not do anything until both File1 and File2 have been specified.
-    if(is.null(inFile1) | is.null(inFile2) )
-      return(NULL)
 
+# Load both source files 
+#    inFile1 <<- input$file1
+#    inFile2 <<- input$file2
     #TODO Confirm these two steps
-    file.rename(inFile1$datapath,
-               paste(inFile1$datapath, ".ttl", sep=""))
-    file.rename(inFile2$datapath,
-               paste(inFile2$datapath, ".ttl", sep=""))
-    
-   query = 'PREFIX cdiscpilot01: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/cdiscpilot01#>
+#    file.rename(inFile1$datapath,
+#               paste(inFile1$datapath, ".ttl", sep=""))
+#    file.rename(inFile2$datapath,
+#               paste(inFile2$datapath, ".ttl", sep=""))
+#   sourceA = load.rdf(paste(inFile1$datapath,".ttl",sep=""), format="N3")
+#   sourceB = load.rdf(paste(inFile2$datapath,".ttl",sep=""), format="N3")
+
+
+
+server <- function(input, output) {
+ 
+    query <- reactive({
+
+
+        qPt1 <- 'PREFIX cdiscpilot01: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/cdiscpilot01#>
 PREFIX custom: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/custom#>
 PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX study:  <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/study#>
-
-SELECT ?s ?p ?o
-WHERE { cdiscpilot01:Person_1 ?p ?o . 
-  BIND("cdiscpilot01:Person_1" as ?s)
-}'
-   sourceA = load.rdf(paste(inFile1$datapath,".ttl",sep=""), format="N3")
-   # Global assign for trouble shooting
-   triplesA <<- as.data.frame(sparql.rdf(sourceA, query))
+PREFIX stuy:  <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/study#>
+SELECT ?s ?p ?o '
    
-   sourceB = load.rdf(paste(inFile2$datapath,".ttl",sep=""), format="N3")
-   triplesB <<- as.data.frame(sparql.rdf(sourceB, query))
+    qPt2 <- paste0("WHERE { ", input$startTriple, " ?p ?o ." ,
+       "BIND('", input$startTriple, "' as ?s  }")  
 
-   inANotB<<-anti_join(triplesA, triplesB)
-   inBNotA<<-anti_join(triplesB, triplesA)
+    query <- paste0(qPt1, qPt2)   
+    })
+    
+   # Global assign for trouble shooting
+   # triplesTW <<- as.data.frame(sparql.rdf(sourceA, query))
+   
+#   triplesAO <<- as.data.frame(sparql.rdf(sourceB, query))
+
+#   inANotB<<-anti_join(triplesTW, triplesAO)
+#   inBNotA<<-anti_join(triplesAO, triplesTW)
    
    # output$inANotB <- renderTable({inANotB})
-   inANotB
+#   inANotB
    # TO ADD
    # output$inBNotA <- renderTable({inBNotA})
-    })
+#    })
+# query <-input$startTriple
+# output$query <- renderText(paste0(input$startTriple))    
+
+    # Note use of query(), not query.
+    output$query <- renderText(query())    
 }
 
+#------------------------------------------------------------------------------
+# UI 
+#------------------------------------------------------------------------------
 ui <- fluidPage(
-  fileInput('file1', 'Choose TTL File A'),
-  fileInput('file2', 'File B'),
-  # visNetworkOutput("network",height = "500px"),
-  # textOutput('text1'),
-  tableOutput('contents')
+  titlePanel("SPARQL query"),
+  fluidRow(
+      textInput("startTriple", label = h4("Parent Triple"), value="cdiscpilot01:Person_1")
+  ),
+  fluidRow(
+      h3(textOutput('query', container = span))    
+  )
+  
+  # textOutput('query'),
+  # tableOutput('contents')
 )
+
+
 shinyApp(ui = ui, server = server)
 
