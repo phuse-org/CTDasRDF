34459fd99eb7caecda5058c01e8b75e5fddbfc70
NovasTaylor <NovasTaylor@gmail.com>
2017-05-17 14:38:20 -0400

    Added display of triplesOnt and triplesR for visual comp of all source triples

--- r/validation/CompTriples-Shiny.R
+++ r/validation/CompTriples-Shiny.R
@@ -7,15 +7,10 @@
 # IN  : Browse to two .TTL files for comparison
 # OUT : ShinyApp window
 # REQ : rrdf
-# NOTE: 
-# TODO: **** Add display of second dataframe: InBNotA
-#       * Add label above dataframe display
-#       * Position A/B file section side-by-side
-#       ***** Add intial selection of the parent Object & consturct that query
-#         on the fly. paste0 of the parameter. do not execute until that param
-#         is present (add to the null determination?) and refire every time
-#         that value changes.    
-#       * add code from Compare.TTL to remove definition, skos:note, etc. from comparison
+# NOTE: Includes display of the triples available from Ont,R, not just the ones
+#         that do not match.
+# TODO: 
+#       
 # 
 ###############################################################################
 library(plyr)    #  rename
@@ -26,34 +21,19 @@ library(shiny)
 setwd("C:/_gitHub/SDTMasRDF/data/rdf")
 server <- function(input, output) {
     output$contents <- renderTable({ 
-    inFileR <<- input$fileR
-    inFileOnt <<- input$fileOnt
-    # Do not do anything until both FileR and FileOnt have been specified.
-    if(is.null(inFileR) | is.null(inFileOnt) )
-      return(NULL)
-
-    #TODO Confirm these two steps
-    file.rename(inFileR$datapath,
-               paste(inFileR$datapath, ".ttl", sep=""))
-    file.rename(inFileOnt$datapath,
-               paste(inFileOnt$datapath, ".ttl", sep=""))
-
-#     
-#         
-#    query = 'PREFIX cdiscpilot01: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/cdiscpilot01#>
-# PREFIX custom: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/custom#>
-# PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-# PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
-# PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-# PREFIX study:  <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/study#>
-# 
-# SELECT ?s ?p ?o
-# WHERE { cdiscpilot01:Person_1 ?p ?o . 
-#   BIND("cdiscpilot01:Person_1" as ?s)
-# }'
+        inFileR <<- input$fileR
+        inFileOnt <<- input$fileOnt
+        # Do not do anything until both FileR and FileOnt have been specified.
+        if(is.null(inFileR) | is.null(inFileOnt) )
+            return(NULL)
     
-    
-       query = paste0("PREFIX cdiscpilot01: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/cdiscpilot01#>
+        #TODO Confirm these two steps
+        file.rename(inFileR$datapath,
+            paste(inFileR$datapath, ".ttl", sep=""))
+        file.rename(inFileOnt$datapath,
+            paste(inFileOnt$datapath, ".ttl", sep=""))
+
+        query = paste0("PREFIX cdiscpilot01: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/cdiscpilot01#>
 PREFIX custom: <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/custom#>
 PREFIX code:  <https://github.com/phuse-org/SDTMasRDF/blob/master/data/rdf/code#>
 prefix owl:   <http://www.w3.org/2002/07/owl#>
@@ -66,29 +46,30 @@ SELECT ?s ?p ?o
 WHERE {", input$qnam, " ?p ?o . 
   BIND(\"", input$qnam, "\" as ?s) } ")
 
-    foo <<- query
+       sourceR = load.rdf(paste(inFileR$datapath,".ttl",sep=""), format="N3")
+       # Global assign for trouble shooting
+       triplesR <<- as.data.frame(sparql.rdf(sourceR, query))
+       
+       sourceOnt = load.rdf(paste(inFileOnt$datapath,".ttl",sep=""), format="N3")
+       triplesOnt <- as.data.frame(sparql.rdf(sourceOnt, query))
     
-   sourceR = load.rdf(paste(inFileR$datapath,".ttl",sep=""), format="N3")
-   # Global assign for trouble shooting
-   triplesR <<- as.data.frame(sparql.rdf(sourceR, query))
-   
-   sourceOnt = load.rdf(paste(inFileOnt$datapath,".ttl",sep=""), format="N3")
-   triplesOnt <- as.data.frame(sparql.rdf(sourceOnt, query))
-
-   # Remove cases where O is missing in the Ontology source(atrifact from TopBraid)
-   triplesOnt <-triplesOnt[!(triplesOnt$o==""),]
-   triplesOnt <<- triplesOnt[complete.cases(triplesOnt), ]
-   if (input$comp=='inRNotOnt') {
-       compResult <<-anti_join(triplesR, triplesOnt)
-   }
-   else if (input$comp=='inOntNotR') {
-       compResult <- anti_join(triplesOnt, triplesR)
-   }
-   compResult
-})
-#TESTING    
-   
+       # Remove cases where O is missing in the Ontology source(atrifact from TopBraid)
+       triplesOnt <-triplesOnt[!(triplesOnt$o==""),]
+       triplesOnt <<- triplesOnt[complete.cases(triplesOnt), ]
+       if (input$comp=='inRNotOnt') {
+           compResult <<-anti_join(triplesR, triplesOnt)
+       }
+       else if (input$comp=='inOntNotR') {
+           compResult <- anti_join(triplesOnt, triplesR)
+       }
+       compResult
+    })
+    # sort for visual compare in the interface
+    triplesOnt <- triplesOnt[with(triplesOnt, order(s,p,o)), ]
+    triplesR <- triplesR[with(triplesR, order(s,p,o)), ]
     
+    output$triplesOnt <-renderTable({triplesOnt})    
+    output$triplesR <-renderTable({triplesR})    
 }
 
 ui <- fluidPage(
@@ -102,7 +83,14 @@ ui <- fluidPage(
   radioButtons("comp", "Compare:",
                 c("In R, not in Ontology" = "inRNotOnt",
                   "In Ontology, not in R" = "inOntNotR")),    
-  tableOutput('contents')
+  h4("Comparison Result:"),
+  hr(),    
+  tableOutput('contents'), 
+  h4("Ontology Triples"),
+  tableOutput('triplesOnt'),
+  h4("R Triples"),
+  tableOutput('triplesR')
+    
 )
 shinyApp(ui = ui, server = server)
 
