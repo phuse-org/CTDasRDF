b916a12455b9d35cde93667a38bfb9b913842df0
NovasTaylor <NovasTaylor@gmail.com>
2017-07-10 16:33:01 -0400

    commit and push prior to merge

--- r/vis/ProgramFlow-Mermaid.R
+++ r/vis/ProgramFlow-Mermaid.R
@@ -14,43 +14,47 @@ library(DiagrammeR)
 mermaid("
 graph TB
     idStart>Start]
-    idMain1(buildRDF-Driver.R1)
-    idMain2(buildRDF-Driver.R2)
-    idMain3(buildRDF-Driver.R3)
-    idMain4(buildRDF-Driver.R4)
-    idMain5(buildRDF-Driver.R5)
-    idMain6(buildRDF-Driver.R6)
-    idMain7(buildRDF-Driver.R7)
-    idMain8(buildRDF-Driver.R8)
-    idMain9(buildRDF-Driver.R9)
-    idMain10(buildRDF-Driver.R10)
-    idMain11(buildRDF-Driver.R11)
-    idMain12(buildRDF-Driver.R12)
-    idMain13(buildRDF-Driver.R13)
-    idMain14(buildRDF-Driver.R14)
+    idMain1(1. buildRDF-Driver.R)
+    idMain2(2. buildRDF-Driver.R)
+    idMain3(3. buildRDF-Driver.R)
+    idMain4(4. buildRDF-Driver.R)
+    idMain5(5. buildRDF-Driver.R)
+    idMain6(6. buildRDF-Driver.R)
+    idMain7(7. buildRDF-Driver.R)
+    idMain8(8. buildRDF-Driver.R)
+    idMain9(9. buildRDF-Driver.R)
+    idMain10(10. buildRDF-Driver.R)
+    idMain11(11. buildRDF-Driver.R)
+    idMain12(12. buildRDF-Driver.R)
+    idMain13(13. buildRDF-Driver.R)
+    idMain14(14. buildRDF-Driver.R)
 
 
     idPrefixes(prefixes.csv)
     idMeta(graphMeta.R)
     idMiscF(misc_F.R)
-    idFntreadXPT{readXPT}
-    idFntaddPersonId{addPersonId}
-    idFntassignDateType{assignDateType}
+    idFntreadXPT((readXPT))
+    idFntaddPersonId((addPersonId))
+    idFntassignDateType((assignDateType))
     idDMxpt((DM.xpt))
     idDMImpute(DM_impute.R)
     idVSxpt((VS.xpt))
     idVSImpute(VS_impute.R)
 
     idCreateFrag(createFrag_F.R)
-    idFntaddDateFrag{addDateFrag}
-    idFntCreateDateDict{createDateDict}
-    idFntcreateFragOneDomain{createFragOneDomain}
+    idFntaddDateFrag((addDateFrag))
+    idFntCreateDateDict((createDateDict))
+    idFntcreateFragOneDomain((createFragOneDomain))
 
-    idDMProcess(processDM.R)
-    idPSDM(processSUPPDM.R)
-    idVSProcess(processVS.R)
+    idDMFrag(DM_Frag.R)
+    idVSFrag(DS_Frag.R)
+
+    idDMProcess(DM_process.R)
+    idSUPPDMProcess(SUPPDM_process.R)
+    idVSProcess(VS_process.R)
  
     idOutMain(CDISCPILOT01-R.TTL)
+    idRiot(Apache RIOT)
     idFin>Fin]
 
     idStart-->idMain1
@@ -58,67 +62,78 @@ graph TB
     idMain1-->idMain2
     idMeta--SOURCED_BY-->idMain2
     idMiscF--SOURCED_BY-->idMain2
-    idFntreadXPT-->idMiscF
-    idFntaddPersonId-->idMiscF
-    idFntassignDateType-->idMiscF
+    idFntreadXPT--DEFINED_IN-->idMiscF
+    idFntaddPersonId--DEFINED_IN-->idMiscF
+    idFntassignDateType--DEFINED_IN-->idMiscF
 
     idMain2-->idMain3
     
     idMain3-->idMain4
-    idFntaddDateFrag-->idCreateFrag
-    idFntcreateFragOneDomain-->idCreateFrag
-    idFntCreateDateDict-->idCreateFrag
+    idFntaddDateFrag--DEFINED_IN-->idCreateFrag
+    idFntcreateFragOneDomain--DEFINED_IN-->idCreateFrag
+    idFntCreateDateDict--DEFINED_IN-->idCreateFrag
     idCreateFrag--SOURCED_BY-->idMain3
 
     idMain4-->idMain5
-    idDMXpt--READ_BY-->idMain5
-
+    idDMxpt--READ_BY-->idMain5
 
     idMain5-->idMain6
-    idDMImpute-->idMain6
-
+    idDMImpute--SOURCED_BY-->idMain6
 
     idMain6-->idMain7
-    
-    
-    idMain7-->idMain8
+    idVSxpt--READ_BY-->idMain7
 
+    idMain7-->idMain8
+    idVSImpute--SOURCED_BY-->idMain8
 
     idMain8-->idMain9
+    idDMFrag--SOURCED_BY-->idMain9
 
     idMain9-->idMain10
-
+    idDMProcess--SOURCED_BY-->idMain10
 
     idMain10-->idMain11
-
+    idSUPPDMProcess--SOURCED_BY-->idMain11
 
     idMain11-->idMain12
-
+    idVSFrag--SOURCED_BY-->idMain12
 
     idMain12-->idMain13
-
+    idVSProcess--SOURCED_BY-->idMain13
 
     idMain13-->idMain14
+    idMain14--WRITES-->idOutMain
+    idRiot--VALIDATES-->idOutMain
+    idOutMain-->idFin
 
 
-    idMain14-->idFin
+  classDef csv      fill:white,stroke:#8282ee,stroke-width:3px;
+  classDef xpt      fill:#bed6d0, stroke:#003263,stroke-width:3px, font-color:white;
 
 
+  classDef main     fill:#7aeb7a,stroke:#000000,stroke-width:3px;
+  classDef sourced  fill:#bdf5bd,stroke:#666600,stroke-width:3px;
+  classDef outTTL   fill:#4ba6ff,stroke:#80aea3,stroke-width:3px;
+  classDef fnt      fill:white,  stroke:#38e138,stroke-width:3px,stroke-dasharray: 5, 5;;
+  classDef validate fill:#ff8080,stroke:black,stroke-width:3px;
 
-  classDef main     fill:#ffff1a, stroke:#000000,stroke-width:3px;
-  classDef sourced  fill:#ffff99, stroke:#666600,stroke-width:3px;
-  classDef xpt      fill:#b3b3ff, stroke:#0000cc,stroke-width:3px;
-  classDef outTTL   fill:#ff6666, stroke:#000000,stroke-width:3px;
-  classDef fnt      fill:#ffe680, stroke:#ffd11a,stroke-width:3px;
+  classDef RImpute  fill:#bdf5bd,stroke:#ff0000,stroke-width:4px;
+  classDef RFrag    fill:#bdf5bd,stroke:#ffa500,stroke-width:4px;
+  classDef RProcess fill:#bdf5bd,stroke:#0000ff,stroke-width:4px;
 
-  classDef terminus fill:lightgreen,stroke:#000000,stroke-width:3px;
-
+  classDef start fill:lightgreen,stroke:#000000,stroke-width:3px;
+  
+  class idStart, start;
   class idMain1,idMain2,idMain3,idMain4,idMain5,idMain6,idMain7,idMain7,idMain8,idMain9,idMain10,idMain11,idMain12,idMain13,idMain14 main;
-  class idFntDataImport,idFntaddDateFrag fnt;
-  class idMeta,idMiscF,idCreateFrag,idDMProcess,idPSDM,idVSProcess sourced;
+  class idFntreadXPT,idFntaddPersonId,idFntassignDateType,idFntaddDateFrag,idFntcreateFragOneDomain,idFntCreateDateDict fnt;
+  class idMeta,idMiscF,idDMProcess,idPSDM,idVSProcess sourced;
   class idDMxpt,idVSxpt xpt;
+  class idDMImpute,idVSImpute RImpute;
+  class idCreateFrag,idDMFrag,idVSFrag RFrag;
+  class idDMProcess,idSUPPDMProcess,idVSProcess RProcess;
   class idOutMain outTTL;
-  class idPrefixes terminus;
+  class idPrefixes csv;
+  class idRiot validate;
 
 ")
 
