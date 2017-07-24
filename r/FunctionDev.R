b2eebf3c52aeb288a568d8eed853d22a2acd1bf3
NovasTaylor <NovasTaylor@gmail.com>
2017-07-24 14:29:16 -0400

    WIP

--- r/FunctionDev.R
+++ r/FunctionDev.R
@@ -1,5 +1,6 @@
 # FunctionDev.R
 # developing : createFragOneColByCat<-function(domainName, dataCol, byCol, fragPrefixCol, numSort=FALSE)
+library(plyr)
 library(dplyr)
 
 vsTest <- read.table(header=T, text='
@@ -20,8 +21,6 @@ vsTest <- read.table(header=T, text='
                61       HEIGHT   HeightOutcome          K2
                  ')
 
-
-
 createFragOneColByCat<-function(domainName, dataCol, byCol, fragPrefixCol, numSort=FALSE)
 {
   temp <- domainName[,c(byCol, dataCol)]
@@ -29,28 +28,20 @@ createFragOneColByCat<-function(domainName, dataCol, byCol, fragPrefixCol, numSo
   # sort by category, data column value
   temp2 <- temp2[ order(temp2[,1], temp2[,2]), ]
   
-  
+  # Create the new column named based on the input column name by appending
+  #  "_Frag" to the value of the the dataCol parameter
   varname <- paste0(dataCol, "_Frag")
 
-  # This works but the id goes away during later steps!!
-  temp2 %>% group_by_(byCol) %>% mutate(id = row_number())%>% 
+  # Note use of !! to resolve the value of varname created above and assign
+  #   a value to it using :=
+  temp2 <- temp2 %>% group_by_(byCol) %>% mutate(id = row_number())%>% 
     mutate( !!varname := paste0(vstestCat,"_", id)) 
   
+  # Remove the ID variable. It is now part of the fragment value in each row
+  temp2 <- temp2[,!(names(temp2) %in% "id")]
   
-  # temp2[[varname]] <- paste0(temp2[,'id'])
-  # temp2[[varname]] <- "test"
-  #temp2 <- temp2
-    # mutate(temp2, !!varname :=paste0(byCol,"_",id))
-  #mutate(temp2, !!varname := id)
-  
-  # Create the new column name as the value of the byCol (vstestCat), _, id
-  # see here, "In the new release" https://stackoverflow.com/questions/26003574/r-dplyr-mutate-use-dynamic-variable-names
-  # mutate_(temp2, .)
-   #fragVarName <- paste0(dataCol,"_Frag")
-   #temp2[[fragVarName]] <- "Test" # paste0(temp2[,1], "_", temp2$id)
-  
+  # Merge the fragment value back into the original data
+  withFrag <<- merge(domainName, temp2, by = c(byCol, dataCol), all.x=TRUE)
+
 }
-
-
-
-newData <- createFragOneColByCat(domainName=vsTest, dataCol="vsorres", byCol="vstestCat", fragPrefixCol="vstestCat")
+vsTest <- createFragOneColByCat(domainName=vsTest, dataCol="vsorres", byCol="vstestCat", fragPrefixCol="vstestCat")
