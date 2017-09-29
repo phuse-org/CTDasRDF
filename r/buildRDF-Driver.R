ea3f6997948cc6e526c47d4064b97a5d83e42029
NovasTaylor <NovasTaylor@gmail.com>
2017-09-29 13:22:33 -0400

    RDF-Driver, metadata triples converted to use redland pkg

--- r/buildRDF-Driver.R
+++ r/buildRDF-Driver.R
@@ -18,7 +18,8 @@
 # TODO: 
 #______________________________________________________________________________
 # Configuration and initial setup ---------------------------------------------
-library(rrdf)
+library(rrdf) # to be depreciated in favor of redland
+library(redland)
 library(Hmisc)
 library(plyr)   # plyr must load prior to dplyr
 library(dplyr)  
@@ -43,42 +44,54 @@ version <- "0.0.1"
 # Output filename and location
 outFileMain   = "data/rdf/cdiscpilot01-R.TTL"
 
+#TODO: Replace with redlands initiation
 # Initialize. Includes OWL, XSD, RDF by default.
 cdiscpilot01  = new.rdf()  # The main datafile. Later change name to 'mainTTL" or similar
     # custom = new.rdf()  # customterminology-R.ttl  : NOT CURRENTLY IN USE
     # code   = new.rdf()  # code-R.ttl               : NOT CURRENTLY IN USE
 
-#** Prefixes and OWL imports ----
+
+# new redlands declarations
+# World is the redland mechanism for scoping models
+world <- new("World")
+
+# Storage provides a mechanism to store models; in-memory hashes are convenient for small models
+storage <- new("Storage", world, "hashes", name="", options="hash-type='memory'")
+
+# A model is a set of Statements, and is associated with a particular Storage instance
+cdiscpilot01 <- new("Model", world=world, storage, options="")
+
+#** Prefix Declarations and [optional] OWL imports ----
 source('R/prefixesAndImports.R') # Prefixes and OWL imports
 
 # Graph Metadata ----
 source('R/graphMeta.R') # Graph Metadata
 
 # External Functions ----
-source('R/misc_F.R')  # Data import, personID, etc.
-source('R/createFrag_F.R') # URI fragement Creation. Eg. Date_1, AgeMeasurement_3
+#TW source('R/misc_F.R')  # Data import, personID, etc.
+#TW source('R/createFrag_F.R') # URI fragement Creation. Eg. Date_1, AgeMeasurement_3
 
 # Import and Impute DM --------------------------------------------------------
-dm <- readXPT("dm")
-dm <- head(dm, maxPerson)  # Keep only first maxPerson obs for development
-source('R/DM_impute.R')     # Create values needed for testing. 
+#TW dm <- readXPT("dm")
+#TW dm <- head(dm, maxPerson)  # Keep only first maxPerson obs for development
+#TW source('R/DM_impute.R')     # Create values needed for testing. 
 
 # Import and Impute VS --------------------------------------------------------
-vs <- readXPT("vs")
+#TW vs <- readXPT("vs")
 
 
-source('R/VS_impute.R') 
+#TW source('R/VS_impute.R') 
 
 # Import and Impute other domains ---- : to be added later----------------------
 
 # Create the date translation table from all dates across domains
 #   Needed by both xx_impute and xx_process scripts.
-dateDict<-createDateDict()    
+#TW dateDict<-createDateDict()    
 
 # Create fragment dictionaries that cross domains
 #   Called after all contributing  domains available, since some fragment values 
 #      (eg: dates), cross multiple domains.
-source('R/DM_frag.R')  # Requires prev. import of VS for VS dates used as part 
+#TW source('R/DM_frag.R')  # Requires prev. import of VS for VS dates used as part 
                        #   of DateDict/dateFrag creation
 
 # Domain Processing -----------------------------------------------------------
@@ -87,19 +100,31 @@ source('R/DM_frag.R')  # Requires prev. import of VS for VS dates used as part
 #      including personNum. 
 
 
-source('R/DM_process.R')
-source('R/SUPPDM_process.R')
+#TW source('R/DM_process.R')
+#TW source('R/SUPPDM_process.R')
 
 # VS Domain ----
-source('R/VS_frag.R')
+#TW source('R/VS_frag.R')
 
 
-source('R/VS_process.R')
+#TW source('R/VS_process.R')
 
 # XX Domain (to be added) ---- 
 
 # Write out to TTL ------------------------------------------------------------
-cdiscpilot01 = save.rdf(cdiscpilot01,  filename=outFileMain,   format="TURTLE")   
+# Serialize the model to a TTL file
+serializer <- new("Serializer", world, name="turtle", mimeType="text/turtle")
+
+# Create the prefixes as a last step prior to serialization
+ddply(prefixes, .(prefix), function(prefixes)
+{
+  status <- setNameSpace(serializer, world, 
+    namespace=as.character(prefixes$namespace), prefix=as.character(prefixes$prefix))  
+})
+  
+# Serialize to the file
+status <- serializeToFile(serializer, world, cdiscpilot01, outFileMain)
+
 
 # Validate --------------------------------------------------------------------
 #   Always a good idea to validate, friendo.
--- r/prefixesAndImports.R
+++ r/prefixesAndImports.R
@@ -25,13 +25,27 @@ prefixes <- as.data.frame( read.csv(allPrefix,
 
 ddply(prefixes, .(prefix), function(prefixes)
 {
-  add.prefix(cdiscpilot01,
-    prefix=as.character(prefixes$prefix),
-    namespace=as.character(prefixes$namespace)
-  )
+
+  # BUild these statements: 
+  # status <- setNameSpace(serializer, world, namespace="http://purl.org/dc/elements/1.1/", prefix="dc")  
+  # status <- setNameSpace(serializer, world, namespace="http://foo.bar/", prefix="foo")  
+# status <- setNameSpace(serializer, world, namespace="https://raw.githubusercontent.com/phuse-org/CTDasRDF/master/data/rdf/cdiscpilot01.ttl#", prefix="cdiscpilot01")
+
+  #TW this takes place in DRIVER.R now
+  #status <- setNameSpace(serializer, world, 
+  #  namespace=as.character(prefixes$namespace), prefix=as.character(prefixes$prefix))  
+    
+  
+#DEL Redland adds perfixes automagically
+#TW   add.prefix(cdiscpilot01,
+#TW     prefix=as.character(prefixes$prefix),
+#TW     namespace=as.character(prefixes$namespace)
+#TW   )
+  
+  
   # Create uppercase prefix names for use in add() statements in the 
   #   xx_process.R scripts. 
-  assign(paste0("prefix.",toupper(prefixes$prefix)), prefixes$namespace, envir=globalenv())
+  assign(paste0(toupper(prefixes$prefix)), prefixes$namespace, envir=globalenv())
   # assign(paste0("prefix.",toupper(prefixes[i, "prefix"])), prefixes[i, "namespace"], envir=globalenv()))
 })
 
@@ -40,24 +54,24 @@ ddply(prefixes, .(prefix), function(prefixes)
 #   Create the owl:import statements 
 #   only if flag in buildRDF-Driver is TRUE
 #------------------------------------------------------------------------------
-if (importsEnabled==TRUE){
-  owlImports <- "data/config/imports.csv"  # List of prefixes
-  imports <- as.data.frame( read.csv(owlImports,
-    header=T,
-    sep=',' ,
-    strip.white=TRUE))
-
-  add.triple(cdiscpilot01,
-    paste0("https://raw.githubusercontent.com/phuse-org/CTDasRDF/master/data/rdf/cdiscpilot01.ttl"),
-    paste0(prefix.RDF,"type" ),
-    paste0(prefix.OWL, "Ontology")
-  )
-  ddply(imports, .(o), function(imports)
-  {
-    add.triple(cdiscpilot01,
-      paste0(imports$s),
-      paste0(prefix.OWL,"imports"),
-      paste0(imports$o)
-    )
-  })
-}
+#TW if (importsEnabled==TRUE){
+#TW   owlImports <- "data/config/imports.csv"  # List of prefixes
+#TW   imports <- as.data.frame( read.csv(owlImports,
+#TW     header=T,
+#TW     sep=',' ,
+#TW     strip.white=TRUE))
+#TW 
+#TW   add.triple(cdiscpilot01,
+#TW     paste0("https://raw.githubusercontent.com/phuse-org/CTDasRDF/master/data/rdf/cdiscpilot01.ttl"),
+#TW     paste0(prefix.RDF,"type" ),
+#TW     paste0(prefix.OWL, "Ontology")
+#TW   )
+#TW   ddply(imports, .(o), function(imports)
+#TW   {
+#TW     add.triple(cdiscpilot01,
+#TW       paste0(imports$s),
+#TW       paste0(prefix.OWL,"imports"),
+#TW       paste0(imports$o)
+#TW     )
+#TW   })
+#TW }
