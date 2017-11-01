5699c3d57a902a3a9421c3e3873d6a77412fba26
NovasTaylor <NovasTaylor@gmail.com>
2017-11-01 13:29:20 -0400

    Adding EX domain shell programs

---
+++ r/EX_frag.R
@@ -0,0 +1,16 @@
+#______________________________________________________________________________
+# FILE: EX_frag.R
+# DESC: 1) Data recoding 
+#       2) URI fragment creation from existing domain values
+# 
+# REQ : 
+# SRC : 
+# IN  : 
+# OUT : 
+# NOTE: No value creation, only recoding. Original values retained in DF
+#       Coded values cannot have spaces or special characters.
+#       SDTM numeric codes and others set MANUALLY
+# TODO: 
+#
+#______________________________________________________________________________
+
