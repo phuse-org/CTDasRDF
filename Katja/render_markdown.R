c062a089d60dc0152744f0ebda0639ca04241c58
Katja Glass <katja.glass@gmail.com>
2018-10-18 08:46:21 +0200

    setup playground

---
+++ Katja/render_markdown.R
@@ -0,0 +1,10 @@
+#install.packages("rmarkdown")
+#library(rmarkdown)
+#library(knitr)
+#knit('Katja/ProcessFlow.rmd', encoding = 'UTF-8')
+
+rmarkdown::render('Katja/HandsOnCTDasRDF.rmd')
+rmarkdown::render('HandsOnCTDasRDF.rmd')
+rmarkdown::render('CTDasRDF.rmd')
+
+#library('readr')
