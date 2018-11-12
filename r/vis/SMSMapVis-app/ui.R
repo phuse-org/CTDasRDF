29e0172e2e93151a5a82f36c9c968743d5b6b934
NovasTaylor <novastaylor@gmail.com>
2018-11-12 13:07:34 -0500

    fix  header comment

--- r/vis/SMSMapVis-app/ui.R
+++ r/vis/SMSMapVis-app/ui.R
@@ -1,34 +1,19 @@
 #______________________________________________________________________________
 # FILE: r/vis/SMSMapVis-app/ui.r
 # DESC: UI for visualization of SMS maps 
+#       User can choose: 
+#        1. The map file(s) to display
+#        2. The namespaces within map files (NOT YET IMPLMENTED)
 # SRC :
 # IN  : 
 # OUT : 
 # REQ : 
 # SRC : 
 # NOTE: 
-# TODO: 
-#______________________________________________________________________________
-
-#______________________________________________________________________________
-# $HeadURL: http://brasvnap001/prod-gsp/oaa/LinkedData/RBMProgramFlow/r/vis/MacroImpactKRI-app/ui.R $
-# $Rev: 393 $
-# $Date: 2018-10-02 16:36:05 -0400 (Tue, 02 Oct 2018) $
-# $Author: U041939 $
-# _____________________________________________________________________________
-# DESC: UI for SMS Map visualizatoin
-#       User can choose: 
-#        1. The map file(s) to display
-#        2. The namespaces within map files (NOT YET IMPLMENTED)
-# SRC : 
-# IN  :
-# OUT :
-# REQ :
-# NOTE: 
 # REF:  Here for late formating of checkbox group inputs to include HTML and images
 #        https://stackoverflow.com/questions/46354705/html-formatting-of-checkboxs-icons-and-text-for-shiny
 # TODO: 
-# _____________________________________________________________________________
+#______________________________________________________________________________
 fluidPage(
   
   
