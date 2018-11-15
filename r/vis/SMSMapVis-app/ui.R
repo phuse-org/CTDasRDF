22e2bcca75a76775c34fa7cce13789dc18221cb4
NovasTaylor <NovasTaylor@gmail.com>
2018-11-15 14:36:07 -0500

    SMS Vis path independence, documentation file.

--- r/vis/SMSMapVis-app/ui.R
+++ r/vis/SMSMapVis-app/ui.R
@@ -1,22 +1,21 @@
 #______________________________________________________________________________
 # FILE: r/vis/SMSMapVis-app/ui.r
-# DESC: UI for visualization of SMS maps 
+# DESC: SMS Visualization App
 #       User can choose: 
 #        1. The map file(s) to display
-#        2. The namespaces within map files (NOT YET IMPLMENTED)
+#        2. The namespaces within map files to exclude
 # SRC :
 # IN  : 
 # OUT : 
 # REQ : 
 # SRC : 
 # NOTE: 
-# REF:  Here for late formating of checkbox group inputs to include HTML and images
+# REF:  For later: format checkbox group inputs using HTML and images
 #        https://stackoverflow.com/questions/46354705/html-formatting-of-checkboxs-icons-and-text-for-shiny
 # TODO: 
 #______________________________________________________________________________
 fluidPage(
-  
-  
+
   tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
     #DEBUG,
@@ -33,7 +32,7 @@ fluidPage(
           "VS"       = "VS",
           "Invest"   = "Invest",
           "Metadata" = "Graphmeta"),
-          selected   = 'DM'),
+          selected   = list('DM','SUPPDM','EX','VS','Invest','Graphmeta' )),
       checkboxGroupInput("namespaces", HTML("<font class='exclude'>Exclude</font> Name Spaces:"),
         c("cdiscpilot01 (blue)" = "cdiscpilot01:",
           "cd01p (green)"       = "cd01p:",
@@ -48,7 +47,7 @@ fluidPage(
       tabsetPanel( type = "tabs",
         tabPanel( "Plot",
           width=10,
-          visNetworkOutput("path_vis", height = '800px', width='800px')
+          visNetworkOutput("path_vis", height = '800px', width='1500px')
         ),
         tabPanel("Triples",
           width=10,
