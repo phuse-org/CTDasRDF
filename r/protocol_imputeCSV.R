bfc039807b1533e1dbae1747eac9ff34ff05e829
NovasTaylor <novastaylor@gmail.com>
2018-12-21 10:08:14 -0500

    primary and secondary OBJ

--- r/protocol_imputeCSV.R
+++ r/protocol_imputeCSV.R
@@ -8,7 +8,9 @@
 # IN  :  
 # OUT : protocol dataframe  
 #       
-# NOTE:  
+# NOTE:  The constructed dataframe is an inefficient structure, with many 
+#         repeated values, but the frame is small so it is acceptabel for 
+#         this intiial POC until a better method is employed.
 # TODO:   
 #______________________________________________________________________________
 #
@@ -19,22 +21,52 @@
   library(dplyr)  # recode, mutate with pipe in Functions.R, other dplyr goodness
 # End lines to be removed
 
-protocol <- data.frame(
-  studyid          = "CDISCPILOT01",
+  
+library(rowr)  # cbind.fill  - add to main driver code
+  
+  
+singleRow <- data.frame(
+
+  actualPopSize    = "254",
   adaptiveDesignNY = "N",
   addOnNY          = "N",
+  ageGroup         = c("ADULT", "ELDERLY"),  
   blindingSchema   = "DOUBLE_BLIND",
   controlType      = "PLACEBO",
+  maxSubjAge       = "NULL",
+  minSubjAge       = "P50Y",
+  plannedPopSize   = "300",
+  sdtmtermNY       = "N",
+  sexGroup         = "BOTH",
+  siteid           = "710",
   studyBegin       = "2012-07-06",
   studyEnd         = "2015-03-05",
-  actualPopSize    = "254",
-  plannedPopSize   = "300",
-  ageGroup         = "ADULT",
-  siteid           = "710",
-  sdtmtermNY       = "N",
-  sexGroup         = "BOTH"
+  studyid          = "CDISCPILOT01"
+  
 )
   
+studyPop <- data.frame(
+      studyPop = c("ADULT", "ELDERLY")
+)
+
+primObj <- data.frame(
+  primObjSeq = c("1", "2", "3"),
+  primObj    = c("To determine if there is a statistically significant relationship between the change in both ADAS-Cog and CIBIC+ scores, and drug dose (0, 50 cm2 [54 mg], and 75 cm2 [81 mg])",
+                       "To document the safety profile of the xanomeline TTS.",
+                       "Evaluate the efficacy and safety of transdermal xanomeline, 50cm2 and 75cm2, and placebo in subjects with mild to moderate Alzheimer's disease."
+                )
+)
+
+secObj <- data.frame(
+  secObjSeq        = c("1", "2", "3", "4"),
+  secObj           = c("To assess the dose-dependent improvement in behavior. Improved scores on the Revised Neuropsychiatric Inventory (NPI-X) will indicate improvement in these areas.",
+                       "To assess the dose-dependent improvements in activities of daily living. Improved scores on the Disability Assessment for Dementia (DAD) will indicate improvement in these areas.",
+                       "To assess the dose-dependent improvements in an extended assessment of cognition that integrates attention/concentration tasks. The ADAS-Cog (14) will be used for this assessment.",
+                       "To assess the treatment response as a function of Apo E genotype."
+                        )
+)
+protocol  <- cbind.fill(singleRow, studyPop, primObj, secObj)
+
 # Sort column names in the df for quicker referencing
 protocol <- protocol %>% select(noquote(order(colnames(protocol))))
 
