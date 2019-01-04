b22efcda4537a73dba3de2f012651021a201e333
cayburn <katja.glass@gmail.com>
2019-01-04 10:54:44 +0100

    TS HandsOnDocumentation Update

--- Katja/render_markdown.R
+++ Katja/render_markdown.R
@@ -6,10 +6,11 @@
 .libPaths("H:/Personal Data/R/win-library/3.5")
 library(rmarkdown)
 library(knitr)
+setwd("C:/Temp/git/CTDasRDF")
 #knit('Katja/ProcessFlow.rmd', encoding = 'UTF-8')
 
 rmarkdown::render('Katja/HandsOnCTDasRDF.rmd')
-rmarkdown::render('HandsOnCTDasRDF.rmd')
+rmarkdown::render('doc/HandsOnCTDasRDF.rmd')
 rmarkdown::render('CTDasRDF.rmd')
 
 #library('readr')
